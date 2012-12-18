cf41a2bb1a4b675eb2c619e6be4843ee54443b97
Ian Whitlock <ian@thewhitlockian.com>
2012-12-18 15:15:21 -0500

    moved to mydns.c, not using libevent anymore.

--- mydns.c
+++ mydns.c
@@ -1,19 +1,42 @@
-#include <event2/util.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-void callback(int result, char type, int count, int ttl, void *addresses, void *arg) {  
+int main (void) {
+	printf("%s%c%c\n","Content-Type:text/html;charset=iso-8859-1\n",13,10);
+  char* get = getenv("QUERY_STRING");
+  if (get == NULL ) {
+  	printf("no query string\n");
+   	return 0;
+  }
 
-}
+	else {
+		char* hostname = (char*)malloc(strlen(get)*sizeof(char));
+		int i;
+		if (i = sscanf(get, "hostname=%s", hostname) < 1 ) {
+        		puts("No addresses given.");
+        		return 0;
+  	}
+		pid_t pid = fork();
+		if ( pid == 0 ) { //child
+	//		printf("in the child. hostname = %s",hostname);
+			char* args[] = {"host","-a",hostname, NULL};
+			printf("%s\n%s\n%s\n",args[0],args[1],args[2]);
+			execvp(args[0], args);
+		} else if ( pid > 0 ) {
+			int status;
+			wait(NULL);
+		//	waitpid(pid, &status, 0);
+	//		printf("The input is: %s",hostname);
 
-int main (int argc, char ** argv) {
-//	evdns_resolv_conf_parse(DNS_OPTIONS_ALL, "/etc/resolv.conf"); 
-	printf("Content-Type: text/plain;charset=us-ascii\n\n");
-	
-	evdns_base_resolve_ipv4("www.theknown.net", 0, callback, NULL); 
+		} else {
+			perror( "fork" );
+			exit(0);
+		}
+	}
 }
 
